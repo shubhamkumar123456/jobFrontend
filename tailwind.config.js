e4f9db835196d6c123537faf27a4204e4fc850cc
shubham kumar <clboy768@gmail.com>
2025-01-05 20:22:59 +0530

    updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flyonui/dist/js/*.js",
-    "./node_modules/flyonui/dist/js/accordion.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [ require("flyonui"),
-    require("flyonui/plugin")],
+  plugins: [],
 }
